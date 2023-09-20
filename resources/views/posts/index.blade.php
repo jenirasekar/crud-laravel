5a1fb99e7905e5aeb18db06cb26f86771c9eba35
Jenira Sekar <jenirasekar@gmail.com>
2023-09-20 12:52:34 +0700

    feat: create index

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Posts - aku.com</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <!-- toastr.js -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 shadow rounded">
+                    <div class="card-body">
+                        <a href="{{ route('posts.create') }}" class="btn btn-md btn-success mb-3">Tambah</a>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Gambar</th>
+                                    <th scope="col">Judul</th>
+                                    <th scope="col">Content</th>
+                                    <th scope="col">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($posts as $post)
+                                <tr>
+                                    <td class="text-center">
+                                        <img src="{{ Storage::url('public/posts/') . $post->image }}" alt="" class="rounded" style="width: 50px;">
+                                    </td>
+                                    <td>{{ $post->title }}</td>
+                                    <td>{{!! $post->content !!}}</td>
+                                    <td class="text-center"></td>
+
+                                    <form action="{{ route('posts.destroy') . $post->id }}" onsubmit="return confirm('Apakah kamu yakin?')" method="post">
+                                        <a href="" class="btn btn-sm btn-primary">Edit</a>
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-sm btn-danger" type="submit">Hapus</button>
+                                    </form>
+                                </tr>
+                                @empty
+                                <div class="alert alert-danger">
+                                    Data Post belum tersedia
+                                </div>
+                                @endforelse
+                            </tbody>
+                        </table>
+                        {{ $posts->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script>
+        @if (session()->has('success')) 
+            toastr.success('{{ session("success") }}', 'Berhasil!');
+         @elseif (session()->has('error'))
+            toastr.error('{{ session("error") }}', 'Gagal!');
+        @endif
+    </script>
+</body>
+
+</html>
