eab2bd0d0e13c188a8508eb507788ac8ed5c6ccb
Jenira Sekar <jenirasekar@gmail.com>
2023-09-20 16:40:20 +0700

    refactor: add some comments

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tambah Data Post - aku.com</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.2/bootstrap.min.css">
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
@@ -14,10 +17,11 @@
             <div class="col-md-12">
                 <div class="card border-0 shadow rounded">
                     <div class="card-body">
-                        <form action="{{ route('posts.store) }}" method="post" enctype="multipart/form-data">
+                        <form action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data">
                             @csrf
+                            <!-- gambar -->
                             <div class="form-group">
-                                <label for="" class="font-weight-bold">Gambar</label>
+                                <label for="" class="fw-bold">Gambar</label>
                                 <input type="file" name="image" id="image" class="form-control" @error('image') is-invalid @enderror>
                                 @error('image')
                                 <div class="alert alert-danger">
@@ -25,8 +29,19 @@
                                 </div>
                                 @enderror
                             </div>
-                            <div class="form-group">
-                                <label for="" class="font-weight-bold">Konten</label>
+                            <!-- judul -->
+                            <div class="form-group mt-3">
+                                <label for="" class="fw-bold">Judul</label>
+                                <input type="text" name="title" id="title" class="form-control" @error('title') is-invalid @enderror placeholder="Masukkan judul post">
+                                @error('title')
+                                <div class="alert alert-danger">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+                            <!-- content -->
+                            <div class="form-group mt-3">
+                                <label for="" class="fw-bold">Konten</label>
                                 <textarea name="content" id="content" rows="5" class="form-control" @error('content') is-invalid @enderror placeholder="Masukkan kontent Post">{{ old('content') }}</textarea>
                                 @error('content')
                                 <div class="alert alert-danger">
@@ -34,8 +49,10 @@
                                 </div>
                                 @enderror
                             </div>
-                            <button class="btn btn-md btn-primary" type="submit">Simpan</button>
-                            <button class="btn btn-md btn-primary" type="reset">Reset</button>
+                            <div class="mt-3">
+                                <button class="btn btn-md btn-primary" type="submit">Simpan</button>
+                                <button class="btn btn-md btn-warning" type="reset">Reset</button>
+                            </div>
                         </form>
                     </div>
                 </div>
@@ -43,9 +60,18 @@
         </div>
     </div>
 
+    <!-- bootstrap script -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <!-- jquery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- toastr -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+    <!-- ckeditor -->
+    <script src="https://cdn.ckeditor.com/3.13.1/standard/ckeditor.js"></script>
     <script>
         CKEDITOR.replace('content');
     </script>
